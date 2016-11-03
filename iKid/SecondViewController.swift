93a404b536f61c5a5bbcd3988493f2d14dfe3ad2
Yulong Tan <yulongtan67@gmail.com>
2016-11-02 21:30:40 -0700

    fixed missing identifier

--- iKid/SecondViewController.swift
+++ iKid/SecondViewController.swift
@@ -9,17 +9,74 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    private var secondViewController : SecondViewController!
+    private var secondJokeViewController : SecondJokeViewController!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func switchViews(_ sender: AnyObject) {
+        secondBuilder()
+        secondJokeBuilder()
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        if secondViewController != nil &&
+            secondViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            secondJokeViewController.view.frame = view.frame
+            switchViewController(from: secondViewController, to: secondJokeViewController)
+        }
+        else {
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            secondViewController.view.frame = view.frame
+            switchViewController(from: secondJokeViewController, to: secondViewController)
+        }
+        UIView.commitAnimations()
+    }
+    
+    private func secondBuilder() {
+        if secondViewController == nil {
+            secondViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "Second")
+                as! SecondViewController
+        }
+    }
+    private func secondJokeBuilder() {
+        if secondJokeViewController == nil {
+            secondJokeViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "Second Joke")
+                as! SecondJokeViewController
+        }
+    }
+    
+    private func switchViewController(from: UIViewController?, to: UIViewController?) {
+        if from != nil {
+            from!.willMove(toParentViewController: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+        
+        if to != nil {
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParentViewController: self)
+        }
+    }
+    
+    
+    
 }
 
--- iKid/ThirdViewController.swift
+++ iKid/ThirdViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ThirdViewController: UIViewController {
     
+    private var thirdViewController : ThirdViewController!
+    private var thirdJokeViewController : ThirdJokeViewController!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +23,59 @@ class ThirdViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func switchViews(_ sender: AnyObject) {
+        ThirdBuilder()
+        ThirdJokeBuilder()
+        
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        if thirdViewController != nil &&
+            thirdViewController?.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+            thirdJokeViewController.view.frame = view.frame
+            switchViewController(from: thirdViewController, to: thirdJokeViewController)
+        }
+        else {
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            thirdViewController.view.frame = view.frame
+            switchViewController(from: thirdJokeViewController, to: thirdViewController)
+        }
+        UIView.commitAnimations()
+    }
+    
+    private func ThirdBuilder() {
+        if thirdViewController == nil {
+            thirdViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "Third")
+                as! ThirdViewController
+        }
+    }
+    private func ThirdJokeBuilder() {
+        if thirdJokeViewController == nil {
+            thirdJokeViewController =
+                storyboard?
+                    .instantiateViewController(withIdentifier: "Third Joke")
+                as! ThirdJokeViewController
+        }
+    }
+    
+    private func switchViewController(from: UIViewController?, to: UIViewController?) {
+        if from != nil {
+            from!.willMove(toParentViewController: nil)
+            from!.view.removeFromSuperview()
+            from!.removeFromParentViewController()
+        }
+        
+        if to != nil {
+            self.addChildViewController(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParentViewController: self)
+        }
+    }
+    
     
     
 }
